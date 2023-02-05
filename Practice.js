9e903475a46e578fc3557980f3c83899584f93f9
one <one1bit@bk.ru>
2023-02-05 17:53:20 +0100

    notes practice

--- Practice.js
+++ Practice.js
@@ -1,8 +1,9 @@
 import { keyList } from "./Lists.js";
-
+import * as timer from"./Timer.js";
 function randomElement(notes){
     return Math.floor(Math.random() * notes.length);
 }
+
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
@@ -25,12 +26,14 @@ let index = selectItem.selectedIndex;
 selectItem.addEventListener('change', () => {
     index = selectItem.selectedIndex;
 })*/
-const practiceTime=1000;
+
 let numberOfPractice=5;
 let counter;
 let randomNote;
 let PracticeOn=false;
-
+let textInterval=null;
+let practiceInterval=null;
+let delay=timer.TIME_LIMIT*1000;
 window.onload = function() {
     renderStaff();
     drawSupportLines();
@@ -44,50 +47,105 @@ window.onload = function() {
     }
 }
 
-
+export let rightClick=true;
+export function setRightClick(value) {
+    rightClick = value;
+}
 window.addEventListener("keydown", matching);
 
-function matching(e){
-    if (PracticeOn===true) {
-        if (e.key === keyList.at(randomNote).shortcut) {
-            console.log("Right!");
+function matching(e) {
+    if (PracticeOn === true) {
+        //if (e.key !== keyList.at(randomNote).shortcut) {
+        if (e.key === "1"){
+            clearInterval(practiceInterval);
+            practiceIteration();
+            clearInterval(textInterval);
+            const searchIndex = keyList.findIndex((keyList) => keyList.shortcut === e.key);
+            timer.createTextRight(keyList.at(searchIndex).note);
+            textInterval = setInterval(() => {
+                timer.deleteElements()
+            }, 2000);
         } else {
-            console.log("Wrong!")
+            timer.createTextWrong(keyList.at(randomNote).note);
+            setTimeout(timer.deleteElements, 1000);
+            //if (counter<=numberOfPractice) {
+            //    setTimeout(practiceIteration,timer.timeLeft*1000);
+            //}
+
         }
-        //setTimeout(startPractice, 1000);
-    }
-    else{
+    } else {
         return false
     }
 }
-function drawTimer()
-{
-    return true
+function practiceIteration() {
+
+    if (counter<=numberOfPractice) {
+        counter++;
+        let checkDoubling = randomNote;
+        do {
+            randomNote = randomElement(keyList);
+        } while (randomNote === checkDoubling)
+
+        timer.drawTimer();
+        timer.startTimer();
+
+        drawNote(randomNote);
+    }
+    else
+    {
+        clearInterval(practiceInterval);
+        document.getElementById("app").style.display = 'none';
+        PracticeOn=false;
+    }
 }
 function startPractice(){
+    if (PracticeOn===true) {
+        practiceIteration();
+        counter++;
+        practiceInterval=setInterval(practiceIteration,delay)
+        console.log('42');
+    }
+}
+/*
+function startPractice(){
         if (PracticeOn===true) {
-            drawTimer();
-            randomNote = randomElement(keyList);
+            if (rightClick===false)
+            {
+                startPractice();
+            }
+            let checkDoubling = randomNote;
+            do {
+                randomNote = randomElement(keyList);
+            } while (randomNote === checkDoubling)
+
+            console.log('42');
+            timer.drawTimer();
+            timer.startTimer();
+            //Notes
             if (document.getElementById("practice-type").options.selectedIndex === 0) {
                 drawNote(randomNote);
-            } else if (document.getElementById("practice-type").options.selectedIndex === 1) {
+            }
+            //Chords
+            else if (document.getElementById("practice-type").options.selectedIndex === 1) {
 
-            } else {
+            }
+            else {
                 console.log("Err is in window.onload choosing practice type")
             }
-            counter++;
-            if (counter < numberOfPractice) {
-                setTimeout(startPractice, practiceTime);
-            }
+
             else{
-                PracticeOn=false;
+                setTimeout(()=> {document.getElementById("app").style.display = 'none';
+                PracticeOn=false;}
+
+            , timer.TIME_LIMIT*1000);
             }
         }
         else{
-            return false;
+            return false
         }
 
-}
+
+}*/
 function drawLine(x1,x2,y){
     context.lineWidth = 2;
     context.moveTo(x1, y);
@@ -133,6 +191,8 @@ function drawNote(note) {
         context.drawImage(imgWhole, 250 + 10 * c, 14.5 * c + 14.5 * 2, 44, 44);
     }
 }
+
+
 ///draw all whole notes
 /*
     let nn=31;
