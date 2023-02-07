d804256dd836fce71cf6659f951510ad9eecba88
Xinmeng-Luan <luanxm0903@gmail.com>
2023-02-07 09:33:04 +0100

    ..

---
+++ practice-tab/practice.js
@@ -0,0 +1,313 @@
+import {keyList} from "./lists.js";
+import * as timer from "./timer.js";
+
+function randomElement(notes){
+    return Math.floor(Math.random() * notes.length);
+}
+
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+let imgStaff = new Image();
+imgStaff.src = "../icons/staff2.png";
+let imgWhole = new Image();
+imgWhole.src = "../icons/Whole.png";
+let imgFlat = new Image();
+imgFlat.src = "../icons/flat.png";
+let imgSharp = new Image();
+imgSharp.src = "../icons/sharp.png";
+
+function renderStaff(){
+    context.drawImage(imgStaff,0,60,700,400)
+}
+renderStaff();
+/*
+const selectItem = document.getElementById('practice-type')
+let index = selectItem.selectedIndex;
+selectItem.addEventListener('change', () => {
+    index = selectItem.selectedIndex;
+})*/
+
+let numberOfPractice=5;
+let counter;
+let randomNote;
+let PracticeOn=false;
+let textInterval=null;
+let notesPracticingInterval=null;
+let chordsPracticingInterval=null;
+let delay=timer.TIME_LIMIT*1000;
+let searchIndex;
+window.onload = function() {
+    renderStaff();
+    drawSupportLines();
+    let a = document.getElementById('button-practice');
+    a.onclick = function (){
+        if(PracticeOn===false) {
+            PracticeOn = true;
+            counter = 0;
+            startPracticing();
+        }
+    }
+}
+
+export let rightClick=true;
+export function setRightClick(value) {
+    rightClick = value;
+}
+
+let pianoKeyboard = document.getElementById('piano');
+let buttons = pianoKeyboard.querySelectorAll('button');
+let clickedPianoKey;
+let clickedPianoKeyCheck=false;
+let namePressedNote;
+let nameCorrectNote;
+export function setPracticeOn(bool){
+    PracticeOn=bool;
+}
+Array.from(buttons).forEach(function(button,i){
+    button.addEventListener('click', function(e) {
+        if(PracticeOn===true) {
+            clickedPianoKey = buttons[i].getAttribute("note");
+            clickedPianoKeyCheck = true;
+            matching(e);
+        }
+    })
+});
+
+setInterval(()=> {
+    if (counter-2>numberOfPractice && timer.timeLeft<=-1){
+        clearInterval(notesPracticingInterval);
+        document.getElementById("app").style.display = 'none';
+        PracticeOn=false;
+
+    }
+},100);
+window.addEventListener("keydown", (e)=> {
+    clickedPianoKeyCheck = false;
+    matching(e);
+});
+
+
+
+function dyingKeys(name){
+    document.querySelectorAll(`[note='${name}']`).forEach(function(element) {
+        if (element.classList.contains("key-white")) {
+            element.style.backgroundColor = "rgb(105,154,79)";
+            setTimeout(()=>
+            {element.style.backgroundColor = "rgb(255,255,255)";
+            }, 1500);
+        } else if (element.classList.contains("key-black")) {
+            element.style.backgroundColor = "rgb(7,87,18)";
+            setTimeout(()=>
+            {element.style.backgroundColor = "rgb(44,52,61)";
+            }, 1500);
+        }
+    });
+}
+
+function matching(e) {
+
+
+
+    if (PracticeOn === true) {
+
+        if (clickedPianoKeyCheck===false){
+            searchIndex = keyList.findIndex((keyList) => keyList.shortcut === e.key);
+            namePressedNote = keyList.at(searchIndex).note;
+        }
+        else
+        {
+            namePressedNote=clickedPianoKey;
+        }
+        nameCorrectNote = keyList.at(randomNote).note;
+        console.log(namePressedNote);
+        console.log(nameCorrectNote);
+
+
+        if (namePressedNote === nameCorrectNote){
+            clearInterval(notesPracticingInterval);
+            notesPracticingIteration();
+            timer.createTextRight(nameCorrectNote);
+            //interacting with keyboard right choice
+            dyingKeys(nameCorrectNote);
+        } else {
+            clearInterval(textInterval);
+            timer.createTextWrong(namePressedNote);
+
+            //interacting with keyboard wring choice
+            document.querySelectorAll(`[note='${namePressedNote}']`).forEach(function(element) {
+                if (element.classList.contains("key-white")) {
+                    element.style.backgroundColor = "rgb(171,28,28)";
+                    setTimeout(()=>
+                    {element.style.backgroundColor = "rgb(255,255,255)";
+                    }, 1500);
+                } else if (element.classList.contains("key-black")) {
+                    element.style.backgroundColor = "rgb(98,8,8)";
+                    setTimeout(()=>
+                    {element.style.backgroundColor = "rgb(44,52,61)";
+                    }, 1500);
+                }
+            });
+            dyingKeys(nameCorrectNote);
+        }
+        clearInterval(textInterval);
+
+        textInterval = setInterval(() => {
+            timer.deleteElements()
+        }, 2000);
+    } else {
+        return false
+    }
+}
+function notesPracticingIteration() {
+
+    if (counter<=numberOfPractice) {
+        counter++;
+        let checkDoubling = randomNote;
+        do {
+            randomNote = randomElement(keyList);
+        } while (randomNote === checkDoubling)
+
+        timer.drawTimer();
+        timer.startTimer();
+
+        drawNote(randomNote);
+    }
+    else
+    {
+        clearInterval(notesPracticingInterval);
+        document.getElementById("app").style.display = 'none';
+        PracticeOn=false;
+    }
+}
+function chordsPracticingIteration(notes){
+    if (counter<=numberOfPractice) {
+        counter++;
+        for(let i =0;i<notes.length;i++){
+            drawNote(randomNote);
+        }
+    }
+    else
+    {
+        clearInterval(chordsPracticingInterval);
+        document.getElementById("app").style.display = 'none';
+        PracticeOn=false;
+    }
+}
+function startPracticing(){
+    let chord="C E G#";
+    let notes = chord.split(' ');
+
+
+    if (PracticeOn===true) {
+        //Notes
+        if (document.getElementById("practice-type").options.selectedIndex === 0) {
+            notesPracticingIteration();
+            counter++;
+            notesPracticingInterval=setInterval(notesPracticingIteration,delay)
+            console.log('42');
+        }
+        //Chords
+        else if (document.getElementById("practice-type").options.selectedIndex === 1) {
+            chordsPracticingIteration(notes);
+            counter++;
+            chordsPracticingInterval=setInterval(chordsPracticingIteration,delay)
+            console.log('42');
+        }
+    }
+    else {
+        console.log("Err is in window.onload choosing practice type")
+    }
+
+}
+
+/*
+function startPractice(){
+        if (PracticeOn===true) {
+            if (rightClick===false)
+            {
+                startPractice();
+            }
+            let checkDoubling = randomNote;
+            do {
+                randomNote = randomElement(keyList);
+            } while (randomNote === checkDoubling)
+            console.log('42');
+            timer.drawTimer();
+            timer.startTimer();
+            //Notes
+            if (document.getElementById("practice-type").options.selectedIndex === 0) {
+                drawNote(randomNote);
+            }
+            //Chords
+            else if (document.getElementById("practice-type").options.selectedIndex === 1) {
+            }
+            else {
+                console.log("Err is in window.onload choosing practice type")
+            }
+            else{
+                setTimeout(()=> {document.getElementById("app").style.display = 'none';
+                PracticeOn=false;}
+            , timer.TIME_LIMIT*1000);
+            }
+        }
+        else{
+            return false
+        }
+}*/
+function drawLine(x1,x2,y){
+    context.lineWidth = 2;
+    context.moveTo(x1, y);
+    context.lineTo(x2, y);
+    context.stroke();
+}
+function drawSupportLines(){
+
+    let x=250;
+    drawLine(x,600,26);
+    drawLine(x,600,55);
+    drawLine(x,600,84);
+    drawLine(x,600,258);
+    drawLine(x,600,458);
+    drawLine(x,600,487);
+    /*
+    let yy=29;
+    for(let i=0;i<800/yy;i++){
+        drawLine(430,600,yy*i-3);
+        console.log('42');
+    }*/
+}
+
+function drawNote(note) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(imgStaff, 0, 60, 700, 400)
+
+    drawSupportLines();
+    let c = keyList.at(note).position;
+    console.log(c);
+    if (keyList.at(note).label.includes('#')) {
+        console.log('sharp');
+        if (c < 17) {
+            context.drawImage(imgSharp, 215 + 10 * c, 3 + 14.5 * c, 44, 44);
+        }
+        if (c >= 17) {
+            context.drawImage(imgSharp, 215 + 10 * c, 14.5 * c + 14.5 * 2, 44, 44);
+        }
+    }
+    if (c < 17) {
+        context.drawImage(imgWhole, 250 + 10 * c, 3 + 14.5 * c, 44, 44);
+    }
+    if (c >= 17) {
+        context.drawImage(imgWhole, 250 + 10 * c, 14.5 * c + 14.5 * 2, 44, 44);
+    }
+}
+
+
+// to practice part
+const go_practice_button = document.querySelector('.button_topage');
+go_practice_button.addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+
+
+
---
+++ practice-tab/timer.js
@@ -0,0 +1,153 @@
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 5;
+const ALERT_THRESHOLD = 3;
+import {setPracticeOn} from "./practice.js";
+const COLOR_CODES = {
+    info: {
+        color: "green"
+    },
+    warning: {
+        color: "orange",
+        threshold: WARNING_THRESHOLD
+    },
+    alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD
+    }
+};
+let remainingPathColor = COLOR_CODES.info.color;
+
+export const TIME_LIMIT=10;
+let timePassed = 0;
+export let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+export function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+}
+export function drawTimer()
+{
+    timeLeft=TIME_LIMIT;
+    document.getElementById("app").innerHTML = `
+<div id="timer" class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+         <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+<span id="base-timer-label" class="base-timer__label">
+    ${formatTime(timeLeft)}
+  </span>
+</div>
+`;
+}
+
+export function startTimer() {
+
+    clearInterval(timerInterval);
+    timePassed = 0;
+    timeLeft=TIME_LIMIT;
+    document.getElementById("app").style.display = 'block';
+    timerInterval = setInterval(() => {
+        if (timeLeft<0)
+        {
+            clearInterval(timerInterval);
+            document.getElementById("app").style.display = 'none';
+            setPracticeOn(false);
+
+        }
+        timePassed = timePassed + 1;
+        //if (rightClick===false){
+        //    setRightClick(true);
+        //    clearInterval(timerInterval);
+        //}
+        //else{
+        timeLeft = TIME_LIMIT - timePassed;
+        //}
+
+        document.getElementById("base-timer-label").innerHTML = formatTime(
+            timeLeft+1
+        );
+        setCircleDasharray();
+        setRemainingPathColor(timeLeft);
+
+    }, 1000);
+
+}
+
+function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(warning.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(info.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(warning.color);
+    }
+}
+
+function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+    const circleDasharray = `${(
+        calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+    document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+export function createTextWrong(key) {
+    timeLeft=0;
+    document.getElementById("app2").innerHTML = `
+<div class="text">
+    <p>
+        <span style='color: #d91a1a;'>Wrong!</span>
+        <span>It was:</span>
+        <span style='color:orangered;'> ${key}</span>
+    </p>
+</div>
+`;
+}
+export function createTextRight(key) {
+    timeLeft=0;
+    document.getElementById("app2").innerHTML = `
+<div class="text">
+    <p>
+        <span style='color: #20c720;'>Right!</span>
+        <span>It was: </span>
+        <span style='color:orangered;'> ${key}</span>
+    </p>
+</div>
+`;
+}
+export function deleteElements() {
+    document.getElementById("app2").innerHTML = '';
+}
